080c3c2963e5f625d4db1907bf7d5e8d28923fa0
Elena <elena.vilimaite@gmail.com>
2016-06-07 14:36:26 +0100

    test init

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra/base'
+
+class Battle < Sinatra::Base
+  get '/' do
+    'This is a battle app!'
+    erb(:login)
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
+
---
+++ spec/features/login_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'players login' do 
+	it 'lets players login' do 
+		visit '/'
+	end 
+end 

5212cfe1c39c930ded9075cee72593fad8435861
Kenneth Barrett <barrett.kenneth@gmail.com>
2016-06-07 15:47:02 +0100

    Completed to step 22

--- app.rb
+++ app.rb
@@ -1,12 +1,26 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+  set :session_secret, "My session secret"
+
   get '/' do
     'This is a battle app!'
     erb(:login)
   end
 
+  post '/names' do
+    session[:Player1] = params[:Player1]
+    session[:Player2] = params[:Player2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @Player_1 = session[:Player1]
+    @Player_2 = session[:Player2]
+    erb(:play)
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
-
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,7 +1,26 @@
 require 'spec_helper'
 
-describe 'players login' do 
-	it 'lets players login' do 
+describe 'players login' do
+	it 'lets players login' do
 		visit '/'
-	end 
-end 
+		fill_in :Player1, with: 'Kenneth'
+		fill_in :Player2, with: 'Elena'
+		click_button :Submit
+		expect(page).to have_content 'Kenneth vs. Elena'
+	end
+end
+
+describe 'battle screen' do
+	before(:each) do
+		visit '/'
+		fill_in :Player1, with: 'Kenneth'
+		fill_in :Player2, with: 'Elena'
+		click_button :Submit
+	end
+
+
+	it 'health of players is displayed' do
+		expect(page).to have_content 'Kenneth 100'
+		expect(page).to have_content 'Elena 100'
+	end
+end
